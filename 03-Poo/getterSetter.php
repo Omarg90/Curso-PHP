814d62c19e8930754d3eef867f39f8f9aa19a9a0
Omarg90 <omargarc.90@gmail.com>
2024-12-06 10:45:42 +0100

    Getter y Setter

--- 03-Poo/getterSetter.php
+++ 03-Poo/getterSetter.php
@@ -5,4 +5,56 @@ fuera de la clase para visualizarlas o modificarlas, pero con
 ciertas restricciones y validaciones o sin poder modificarlo
 */
 
+class Asignatura
+{
+    //Accedemos a la propiedad privata $nombre medinate y getNombre;
 
+
+    private int $id;
+    private string $nombre;
+    protected int $idProfesor;
+    protected static array $asignaturas = [];
+
+    public function __construct(int $id, string $nombre, int $idProfesor)
+    {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->idProfesor = $idProfesor;
+        self::$asignaturas[] = $this;
+    }
+
+    public function añadirAsignaturas(int $id, string $nombre, int $idProfesor)
+    {
+        $asignatura = new Asignatura($id, $nombre, $idProfesor);
+        return $asignatura;
+    }
+    public static function verAsignaturas()
+    {
+        foreach (self::$asignaturas as $asignatura) {
+            echo "</br>ID Asignatura: " . $asignatura->id . "</br>" .
+                "Nombre: " . $asignatura->nombre . "</br>" .
+                "ID Profesor: " . $asignatura->idProfesor . "</br>" .
+                "-------------------------------------";
+        }
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    
+}
+
+$asignatura1 = new Asignatura(3, "LND", 1);
+$asignatura2 = new Asignatura(2, "PRO", 7);
+Asignatura::verAsignaturas();
+// Cambiamos el nombre de la asignatura2
+$asignatura2->setNombre("APL");
+echo "</br>".$asignatura1->getNombre()."</br>";
+echo $asignatura2->getNombre()."</br>";
